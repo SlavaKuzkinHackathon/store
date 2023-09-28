17f323d9ef8be1b823de3babfbb0cdaebb52ffa9
SlavaKuzkinHackathon <jess.888@yandex.ru>
2023-09-28 17:29:23 +0600

    28,09,23

---
+++ client/components/elements/CityButton/CityButton.tsx
@@ -0,0 +1,16 @@
+import LocationSvg from '../LocationSvg/LocationSvg'
+
+const CityButton = () => {
+    console.log('');
+    
+  return (
+    <button>
+      <span>
+        <LocationSvg />
+      </span>
+      <span>Новосибирск</span>
+    </button>
+  )
+}
+
+export default CityButton
