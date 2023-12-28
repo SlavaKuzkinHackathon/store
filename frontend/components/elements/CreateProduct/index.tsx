8e5553f043f82823970e78deb82f431cbd6cdb88
SlavaKuzkinHackathon <jess.888@yandex.ru>
2023-12-28 12:26:05 +0600

    28.12-01

--- frontend/components/elements/CreateProduct/index.tsx
+++ frontend/components/elements/CreateProduct/index.tsx
@@ -5,7 +5,7 @@ import { getAuthDataFromLS } from '@/utils/auth'
 import { ChangeEvent, MutableRefObject, useRef, useState } from 'react'
 
 const CreateProduct = () => {
-  const [imageFile, setImageFile] = useState<boolean>(false)
+  const [image, setImage] = useState<File | null>(null);
 
   const nameRef = useRef() as MutableRefObject<HTMLInputElement>
   const descriptionRef = useRef() as MutableRefObject<HTMLInputElement>
@@ -15,16 +15,13 @@ const CreateProduct = () => {
   const imagesRef = useRef() as MutableRefObject<HTMLInputElement>
 
   const formSubmit = async (event: ChangeEvent<HTMLFormElement>) => {
-    
-    event.preventDefault()
-    const file = event.target.files && event.target.files[0]
-    const formData = new FormData()
-    formData.append('images', file)
-    setImageFile(true)
-     
-    
+    event.preventDefault();
 
-    setImageFile(true)
+  
+  /*   const formData = new FormData();
+    event.preventDefault()
+    const file =  event.target.files[0]
+    formData.append('images', file) */
 
     const nameInputValue = nameRef.current.value
     const descriptionInputValue = descriptionRef.current.value
@@ -34,7 +31,6 @@ const CreateProduct = () => {
     const imagesInputValue = imagesRef.current.value
 
     const authData = getAuthDataFromLS()
-
 
     const productCreate = await createProductFx({
       url: '/products',
@@ -47,9 +43,146 @@ const CreateProduct = () => {
         images: imagesInputValue,
       },
       token: authData.access_token,
-    }) 
-/**id ошибка в productList */
+    })
+  //id ошибка в productList 
     //createProduct(productCreate)
+  }
+
+   function handleFileSelect(event: ChangeEvent<HTMLInputElement>): void {
+    const file = event.target.files && event.target.files[0]
+    const formData = new FormData()
+    formData.append('images', file as File)
+  } 
+
+/*   function handleFileSelect(event: ChangeEvent<HTMLInputElement>): void {
+    const file = event.target.files && event.target.files[0]
+    const reader = new FileReader()
+    reader.readAsText(file as File)
+
+    reader.onload = () => {
+      console.log(reader.result)
+    }
+  } */
+
+/*  
+   const file = event.target.files && event.target.files[0]
+    const formData = new FormData()
+    formData.append('images', file)
+    setImageFile(true)
+
+const handleFileSelect = (e: { target: { files: any[] } }) => {
+    const file =  e.target.files[0]
+    const reader = new FileReader()
+    reader.readAsText(file)
+
+    reader.onload = () => {
+      console.log(reader.result)
+    }
+  } */
+
+  return (
+    <form className={styles.form} onSubmit={formSubmit}>
+      <h1>Создать товар</h1>
+      <div className={styles.form_item}>
+        <input
+          ref={nameRef}
+          type="text"
+          placeholder="Наименование"
+          className="form-control"
+        />
+      </div>
+
+      <div className={styles.form_item}>
+        <input
+          ref={descriptionRef}
+          type="text"
+          placeholder="Описание"
+          className="form-control"
+        />
+      </div>
+      <div className={styles.form_item}>
+        <input
+          ref={priceRef}
+          type="number"
+          placeholder="Стоимость"
+          className="form-control"
+        />
+      </div>
+      <div className={styles.form_item}>
+        <input
+          ref={in_stockRef}
+          type="number"
+          placeholder="Количество"
+          className="form-control"
+        />
+      </div>
+      <div className={styles.form_item}>
+        <input ref={ratingRef} placeholder="Рейтинг" className="form-control" />
+      </div>
+      <div className={styles.form_item}>
+        <input
+          ref={imagesRef}
+          onChange={handleFileSelect}
+          type="file"
+          placeholder="Image"
+          className="form-control"
+        />
+      </div>
+      <button>Создать</button>
+    </form>
+  )
+}
+
+export default CreateProduct
+
+
+
+/* import { createProductFx } from '@/app/api/products'
+import { createProduct } from '@/context/products'
+import styles from '@/styles/admin/createProduct.module.scss'
+import { getAuthDataFromLS } from '@/utils/auth'
+import { ChangeEvent, MutableRefObject, useRef, useState } from 'react'
+
+const CreateProduct = () => {
+  const [imageFile, setImageFile] = useState<boolean>(false)
+
+  const nameRef = useRef() as MutableRefObject<HTMLInputElement>
+  const descriptionRef = useRef() as MutableRefObject<HTMLInputElement>
+  const priceRef = useRef() as MutableRefObject<HTMLInputElement>
+  const in_stockRef = useRef() as MutableRefObject<HTMLInputElement>
+  const ratingRef = useRef() as MutableRefObject<HTMLInputElement>
+  const imagesRef = useRef() as MutableRefObject<HTMLInputElement>
+
+  const formSubmit = async (event: ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const file = event.target.files && event.target.files[0]
+    const formData = new FormData()
+    formData.append('images', file)
+    setImageFile(true)
+
+    const nameInputValue = nameRef.current.value
+    const descriptionInputValue = descriptionRef.current.value
+    const priceInputValue = priceRef.current.value
+    const in_stockInputValue = in_stockRef.current.value
+    const ratingInputValue = ratingRef.current.value
+    const imagesInputValue = imagesRef.current.value
+
+    const authData = getAuthDataFromLS()
+
+    const productCreate = await createProductFx({
+      url: '/products',
+      product: {
+        name: nameInputValue,
+        description: descriptionInputValue,
+        price: parseInt(priceInputValue),
+        in_stock: parseInt(in_stockInputValue),
+        rating: parseInt(ratingInputValue),
+        images: imagesInputValue,
+      },
+      token: authData.access_token,
+    })
+  //id ошибка в productList 
+    createProduct(productCreate)
   }
 
   return (
@@ -97,7 +230,6 @@ const CreateProduct = () => {
           type="file"
           placeholder="Image"
           className="form-control"
-          
         />
         {imageFile}
       </div>
@@ -107,3 +239,4 @@ const CreateProduct = () => {
 }
 
 export default CreateProduct
+ */
