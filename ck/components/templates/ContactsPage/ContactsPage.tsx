24946828b81063a9fb5a9126e25167c21d8855a4
SlavaKuzkinHackathon <jess.888@yandex.ru>
2024-02-09 17:19:06 +0600

    09.02-02

--- ck/components/templates/ContactsPage/ContactsPage.tsx
+++ ck/components/templates/ContactsPage/ContactsPage.tsx
@@ -5,7 +5,7 @@ import styles from '@/styles/contacts/index.module.scss'
 import MailSvg from '@/components/elements/MailSvg/MailSvg'
 import FeedbackForm from '@/components/modules/FeedbackForm/FeedbackForm'
 
-const ContactsPage = ({ isWholesaleBuyersPage = false }) => {
+const ContactsPage = () => {
   const mode = useStore($mode)
   const isMobile560 = useMediaQuery(560)
   const darkModeClass = mode === 'dark' ? `${styles.dark_mode}` : ''
@@ -14,23 +14,10 @@ const ContactsPage = ({ isWholesaleBuyersPage = false }) => {
     <section className={styles.contacts}>
       <div className="container">
         <h2 className={`${styles.contacts__title} ${darkModeClass}`}>
-          {isWholesaleBuyersPage ? 'Оптовым покупателям' : 'Контакты'}
+          Контакты
         </h2>
         <div className={styles.contacts__inner}>
-          {isWholesaleBuyersPage ? (
-            <div className={`${styles.contacts__list} ${darkModeClass}`}>
-              <p>
-                <span>
-                  Условия оптовых заказов решаются индивидуально по телефону:{' '}
-                </span>
-                <span>+7 (555) 55-55-555</span>
-              </p>
-              <p>
-                Либо опишите суть заказа в форме обртной связи и мы с вами
-                свяжемся.
-              </p>
-            </div>
-          ) : (
+
             <ul className={`${styles.contacts__list} ${darkModeClass}`}>
               <li className={styles.contacts__list__title}>
                 <h3 className={darkModeClass}>
@@ -51,7 +38,7 @@ const ContactsPage = ({ isWholesaleBuyersPage = false }) => {
               </li>
               <li className={`${styles.contacts__list__item} ${darkModeClass}`}>
                 <span>Наш контактный телефон:</span>
-                <span> +7(8095) 555-55-55</span>
+                <span> +7 (913) 913-55-47</span>
               </li>
               <li className={`${styles.contacts__list__item} ${darkModeClass}`}>
                 <span>Время приемок завок:</span>
@@ -69,7 +56,6 @@ const ContactsPage = ({ isWholesaleBuyersPage = false }) => {
                 </span>
               </li>
             </ul>
-          )}
           <FeedbackForm />
         </div>
       </div>
